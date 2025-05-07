b82399215436d5d2cb6502797675c4de78ff828a
Harshit Singh <harshit.singh@velocis.co.in>
2025-05-07 14:13:14 +0530

    Add Vercel build script

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-# Install Java (OpenJDK 17) and Maven
-apt-get update
-apt-get install -y openjdk-17-jdk maven
+# Create a directory for tools
+mkdir -p tools
+cd tools
 
-# Run Maven to install dependencies and build
+# Download and install OpenJDK 17
+wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/ojdk17.0.2_linux-x64_bin.tar.gz
+tar -xzf ojdk17.0.2_linux-x64_bin.tar.gz
+export JAVA_HOME=$PWD/jdk-17.0.2
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Download and install Maven
+wget https://downloads.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+tar -xzf apache-maven-3.9.9-bin.tar.gz
+export PATH=$PWD/apache-maven-3.9.9/bin:$PATH
+
+# Verify installations
+java -version
+mvn -version
+
+# Run Maven to build the project
+cd ..
 mvn clean install -DskipTests
